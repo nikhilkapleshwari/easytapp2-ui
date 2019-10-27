029878c6abcd9f00ef441887c256be8977bfee04
nikhil kapleshwari <nikhil1632@gmail.com>
2019-10-27 12:45:41 +0530

    background color added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+  
   title = 'EasyTappUI 2';
+
+  constructor(private elementRef: ElementRef){
+
+  }
+  
+  ngAfterViewInit(): void {
+    this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = '#76b852';
+  }
 }
